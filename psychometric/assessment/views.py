d61b8af35c49e982fd776db991e5f35e4068dcc2
Suraj6013 <surajwadkar2021.ainds@mmcoe.edu.in>
2024-07-23 23:22:26 +0530

    chore: Add viewsets and serializers for Assessment, Question, and Response models

--- psychometric/assessment/views.py
+++ psychometric/assessment/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Assessment, Question, Response
+from .serializers import AssessmentSerializer, QuestionSerializer, ResponseSerializer
 
-# Create your views here.
+class AssessmentViewSet(viewsets.ModelViewSet):
+    queryset = Assessment.objects.all()
+    serializer_class = AssessmentSerializer
+
+class QuestionViewSet(viewsets.ModelViewSet):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
+
+class ResponseViewSet(viewsets.ModelViewSet):
+    queryset = Response.objects.all()
+    serializer_class = ResponseSerializer
