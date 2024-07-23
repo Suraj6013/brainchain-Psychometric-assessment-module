52d97442080e1e1d8d99ca67731dfc2700baf3a7
Suraj6013 <surajwadkar2021.ainds@mmcoe.edu.in>
2024-07-23 22:37:32 +0530

    chore: Remove unnecessary files and code related to the assessment app

--- assessment/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Assessment, Question, Option, Response
-
-admin.site.register(Assessment)
-admin.site.register(Question)
-admin.site.register(Option)
-admin.site.register(Response)
